74d13d186e478328d41fa5c0a31b8d518d10cf87
LEEJH1029 <uamybest4@naver.com>
2024-05-02 15:42:56 +0900

    [Init] 프로젝트 생성

---
+++ src/main/java/org/example/likelionkwu/LikelionkwuApplication.java
@@ -0,0 +1,13 @@
+package org.example.likelionkwu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikelionkwuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LikelionkwuApplication.class, args);
+    }
+
+}
